4e7b2c9053c055040246cc377ae30c3f71456710
Laviele Trias <lavieletrias12@gmail.com>
2024-02-02 13:55:16 +0800

    Updated navbar.vue
    Updated code to maximize utilization of Bulma css.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,55 +1,61 @@
 <template>
-  <nav class="navbar">
-    <div class="nametitle" id="#top"><a href="#top">Laviele Trias</a></div>
-    <div :class="['navbar-menu', { 'is-active': isNavbarActive }]">
-      <div class="navbarlinks">
-        <ul>
-          <li><a href="#aboutpage">About Me</a></li>
-          <li><a href="#education">Education</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact Me</a></li>
-        </ul>
-      </div>
-    </div>
-    <!-- <a
-      role="button"
-      class="navbar-burger burger"
-      aria-label="menu"
-      aria-expanded="false"
-      data-target="navbarBasicExample"
-      @click="toggleNavbar"
-    >
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a> -->
-  </nav>
-  <div class="landingpage" id="top">
-    <div class="landingpagewrap">
-      <h1>Hello, world.</h1>
-      <h2>My name is Laviele Trias. I am an aspiring software developer.</h2>
-      <button class="gradientbutton">
-        <span><a href="#contact">Contact Me</a></span>
-      </button>
+<nav class = "navbar">
+  <div class="navbar-brand">
+    <a class="navbar-item">
+      <img src="https://i.imgur.com/H3lKnVi.png" width="30" height="40">
+    </a>
+    <div class="navbar-burger" data-target="navbarlist">
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
   </div>
-</template>
 
-<script>
-export default {
-  // data() {
-  //   return {
-  //     isNavbarActive: false,
-  //   };
-  // },
-  // methods: {
-  //   toggleNavbar() {
-  //     this.isNavbarActive = !this.isNavbarActive;
-  //   },
-  // },
-};
-</script>
+
+  <div id="navbarlist" class="navbar-menu">
+    <div class="navbar-start">
+      <div class="navbar-item">
+        <div class="button is-black">Laviele Trias</div>
+      </div>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+          <p class="control">
+            <a class="button is-black" href="#aboutpage">
+              <span>About Me</span>
+            </a>
+          </p>
+          <p class="control">
+            <a class="button is-black" href="#education">
+              <span>Education</span>
+            </a>
+          </p>
+          <p class="control">
+            <a class="button is-black" href="#skills">
+              <span>Skills</span>
+            </a>
+          </p>
+          <p class="control">
+            <a class="button is-black" href="#contact">
+              <span>Contact Me</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+  <section class="hero is-medium herobg">
+    <div class ="hero-body">
+      <div class ="container">
+        <h1 class="title is-1 herocontent">Hello, world!</h1>
+        <h2 class="subtitle herocontent">My name is Laviele Trias. I am an aspiring software developer.</h2>
+      </div>
+    </div>
+  </section>
+  </template>
 
 <style lang="scss" scoped>
 @import "~bulma/css/bulma.css";
@@ -72,130 +78,18 @@ export default {
   z-index: 999;
 }
 
-.nametitle {
-  font-size: 1.5rem;
-  margin: 2rem;
-  cursor: pointer;
-}
-
-.navbarlinks ul {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  cursor: pointer;
-}
-
-.navbarlinks li {
-  list-style: none;
-}
-
-.navbarlinks li a {
-  text-decoration: none;
-  color: white;
-  padding: 1rem;
-  display: block;
-}
-
-.navbarlinks li :hover {
-  background-image: linear-gradient(144deg, #af40ff, #5b42f3 50%, #00ddeb);
-}
-
-.landingpage {
-  display: flexbox;
-  justify-content: space-between;
-  align-items: center;
-  background: url(../assets/homebg.jpg) no-repeat;
-  background-size: cover;
-  height: 100vh;
-  flex-direction: column;
-}
-
 /* Landing Page */
-.landingpagewrap {
-  font-family: "Roboto", sans-serif;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  color: white;
+
+.herobg{
+  background-image: url('../assets/homebg.jpg'); /* Replace with your image path */
+      background-size: cover;
+      background-position: center center;
+      height: 400px; /* Set a height for the hero section */
+      color: white; /* Adjust text color for better visibility */
 }
 
-.landingpage h1 {
-  margin-bottom: 10px;
-  align-items: left;
-  font-size: 100px;
-  background: -webkit-linear-gradient(144deg, #af40ff, #5b42f3 50%, #00ddeb);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+.herocontent{
+  color:white;
   font-family: "Prompt", sans-serif;
 }
-
-.landingpage h2 {
-  gap: 0;
-  align-items: left;
-  font-size: 25px;
-  font-family: "Palanquin", sans-serif;
-}
-
-/* Gradient Buttons */
-
-.gradientbutton {
-  position: relative;
-  font-family: inherit;
-  font-weight: 500;
-  font-size: 18px;
-  letter-spacing: 0.05em;
-  border-radius: 0.8em;
-  border: none;
-  background: linear-gradient(to right, #8e2de2, #4a00e0);
-  color: ghostwhite;
-  overflow: hidden;
-  width: 10rem;
-  font-family: "Prompt", sans-serif;
-  width: auto;
-  cursor: pointer;
-}
-
-.gradientbutton a {
-  text-decoration: none;
-  color: white;
-}
-
-.gradientbutton span {
-  position: relative;
-  z-index: 10;
-  transition: color 0.4s;
-  display: inline-flex;
-  align-items: center;
-  padding: 0.8em 1.2em 0.8em 1.05em;
-}
-
-.gradientbutton::before,
-.gradientbutton::after {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 0;
-}
-
-.gradientbutton::before {
-  content: "";
-  background: #000;
-  width: 120%;
-  left: -10%;
-  transform: skew(30deg);
-  transition: transform 0.4s cubic-bezier(0.3, 1, 0.8, 1);
-}
-
-.gradientbutton:hover::before {
-  transform: translate3d(100%, 0, 0);
-}
-
-.gradientbutton:active {
-  transform: scale(0.95);
-}
-
 </style>
