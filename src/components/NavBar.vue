b7b32b653eac12a4086a549ecd2b5c8fec894d3f
Laviele Trias <lavieletrias12@gmail.com>
2024-02-03 16:51:50 +0800

    Modified navbar
    Added navbar button modification animation for the navigation bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,30 +14,30 @@
 
   <div id="navbarlist" class="navbar-menu">
     <div class="navbar-start">
-      <div class="navbar-item">
-        <div class="button is-black">Laviele Trias</div>
-      </div>
+      <div class="navbar-item has-text-white">
+          Laviele Trias
+        </div>
     </div>
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="field is-grouped">
           <p class="control">
-            <a class="button is-black" href="#aboutpage">
+            <a class="button is-black gradientbutton" href="#aboutpage">
               <span>About Me</span>
             </a>
           </p>
           <p class="control">
-            <a class="button is-black" href="#education">
+            <a class="button is-black gradientbutton" href="#education">
               <span>Education</span>
             </a>
           </p>
           <p class="control">
-            <a class="button is-black" href="#skills">
+            <a class="button is-black gradientbutton" href="#skills">
               <span>Skills</span>
             </a>
           </p>
           <p class="control">
-            <a class="button is-black" href="#contact">
+            <a class="button is-black gradientbutton" href="#contact">
               <span>Contact Me</span>
             </a>
           </p>
@@ -92,4 +92,63 @@
   color:white;
   font-family: "Prompt", sans-serif;
 }
+
+
+.gradientbutton {
+  position: relative;
+  font-family: inherit;
+  font-weight: 500;
+  letter-spacing: 0.05em;
+  border-radius: 0.8em;
+  border: none;
+  background: linear-gradient(to right, #8e2de2, #4a00e0);
+  color: ghostwhite;
+  overflow: hidden;
+  width: 10rem;
+  font-family: "Prompt", sans-serif;
+  width: 10rem;;
+  cursor: pointer;
+}
+
+.gradientbutton a {
+  text-decoration: none;
+  color: white;
+}
+
+.gradientbutton span {
+  position: relative;
+  z-index: 10;
+  transition: color 0.4s;
+  display: inline-flex;
+  align-items: center;
+  padding: 0.8em 1.2em 0.8em 1.05em;
+}
+
+.gradientbutton::before,
+.gradientbutton::after {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+}
+
+.gradientbutton::before {
+  content: "";
+  background: #000;
+  width: 120%;
+  left: -10%;
+  transform: skew(30deg);
+  transition: transform 0.4s cubic-bezier(0.3, 1, 0.8, 1);
+}
+
+.gradientbutton:hover::before {
+  transform: translate3d(100%, 0, 0);
+}
+
+.gradientbutton:active {
+  transform: scale(0.95);
+}
+
 </style>
